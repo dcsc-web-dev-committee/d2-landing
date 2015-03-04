6c5c095988105a9914f11183aa493bb88e65cb6a
Kelvin Lu <klu@ucdavis.edu>
2015-03-04 12:46:18 -0800

    more skewing

--- js/init.js
+++ js/init.js
@@ -30,26 +30,64 @@
                 enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
             }),
 
-        new ShuntDiv.Transition('about', 'subscribe', 'exitAnimateCss', 'keypress', {
-                key: 40,
-                animation_name: 'fadeOut',
-                animation_function: 'cubic-bezier(.08,.98,.86,.99)',
-            }),
-        new ShuntDiv.Transition('subscribe', 'about', 'exitAnimateCss', 'keypress', {
-                key: 38,
-                animation_name: 'fadeOut',
-                animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+        new ShuntDiv.Transition('video', 'about', 'dualAnimateCss', 'touchSwipe', {
+                swipe: 'up',
+                exit_animation_name: 'fadeOutUp',
+                enter_animation_name: 'fadeInUp',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+            }),   
+        new ShuntDiv.Transition('about', 'video', 'dualAnimateCss', 'touchSwipe', {
+                swipe: 'down',
+                exit_animation_name: 'fadeOutDown',
+                enter_animation_name: 'fadeInDown',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
             }),
 
-        new ShuntDiv.Transition('about', 'subscribe', 'exitAnimateCss', 'click', {
-                id: 'down',
-                animation_name: 'fadeOut',
-                animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+        new ShuntDiv.Transition('about', 'subscribe', 'dualAnimateCss', 'keypress', {
+                key: 40,
+                exit_animation_name: 'fadeOutUp',
+                enter_animation_name: 'fadeInUp',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
             }),
-        new ShuntDiv.Transition('subscribe', 'about', 'exitAnimateCss', 'click', {
+        new ShuntDiv.Transition('subscribe', 'about', 'dualAnimateCss', 'keypress', {
+                key: 38,
+                exit_animation_name: 'fadeOutDown',
+                enter_animation_name: 'fadeInDown',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+            }),
+
+        new ShuntDiv.Transition('about', 'subscribe', 'dualAnimateCss', 'click', {
+                id: 'down',
+                exit_animation_name: 'fadeOutUp',
+                enter_animation_name: 'fadeInUp',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+            }),
+        new ShuntDiv.Transition('subscribe', 'about', 'dualAnimateCss', 'click', {
                 id: 'up',
-                animation_name: 'fadeOut',
-                animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                exit_animation_name: 'fadeOutDown',
+                enter_animation_name: 'fadeInDown',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+            }),
+
+        new ShuntDiv.Transition('about', 'subscribe', 'dualAnimateCss', 'touchSwipe', {
+                swipe: 'up',
+                exit_animation_name: 'fadeOutUp',
+                enter_animation_name: 'fadeInUp',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+            }),
+        new ShuntDiv.Transition('subscribe', 'about', 'dualAnimateCss', 'touchSwipe', {
+                swipe: 'down',
+                exit_animation_name: 'fadeOutDown',
+                enter_animation_name: 'fadeInDown',
+                exit_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
+                enter_animation_function: 'cubic-bezier(.08,.98,.86,.99)',
             }),
     ], {
         default: 'video',
